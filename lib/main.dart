84101669efc5f9a498083d21fd076f714513aee6
miloud1010 <lenouarmiloud@gmail.com>
2019-03-30 20:23:32 +0100

    this is my first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //this method accept widjet as parameter
+  runApp(
+    new Center(
+        child: new Text("مرحبا بكم معنا في أول برنامج عن طريق إستعمال الفلاتر ",
+            textDirection: TextDirection.rtl)),
+  );
+}
